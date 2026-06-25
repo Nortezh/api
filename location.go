e4375d6b0c1ba2350dc96b2c65258be1db1cb78f
targc <sommit.kcc@gmail.com>
2026-06-26 01:11:23 +0700

    feat: database feature flag

--- location.go
+++ location.go
@@ -63,6 +63,7 @@ func (m *LocationItem) Table() [][]string {
 type LocationFeatures struct {
 	WorkloadIdentity bool      `json:"workloadIdentity,omitempty" yaml:"workloadIdentity"`
 	Disk             *struct{} `json:"disk,omitempty" yaml:"disk"`
+	Database         *struct{} `json:"database,omitempty" yaml:"database"`
 }
 
 type LocationGet struct {
